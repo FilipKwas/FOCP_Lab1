f14077e31902226b752751567c9058ea9ffd7a69
FilipKwas <filikwa678@student.polsl.pl>
2020-10-10 13:33:28 +0200

    Example minimum function

--- Project3/Project3/Źródło.cpp
+++ Project3/Project3/Źródło.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <array> 
+#include <cstdlib>
 
-int main () {
-	
-	std::array<int, 10> data;
+int main() {
+
+	std::array<int, 10> data = { 1,4,5,6,3,8,2,9,0,7 };
+
+	int smallest = data[0];
 
 	for (int i = 0; i < 10; i++) {
-		data[i] = i;
+		if (data[i] < smallest) {
+			smallest = data[i];
+		}
 	}
 
-	for (int i = 0; i < 10; i++) {
-	std::cout << data[i] << std::endl;
+	std::cout << "Smallest number is: " << smallest << std::endl;
 
-	}
-	
 	return 0;
 }
