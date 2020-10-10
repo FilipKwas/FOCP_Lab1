912e007c7e4828d83b4b841131ee2e7d9132500c
FilipKwas <filikwa678@student.polsl.pl>
2020-10-10 11:42:56 +0200

    Input/output example

--- Project3/Project3/Źródło.cpp
+++ Project3/Project3/Źródło.cpp
@@ -2,7 +2,20 @@
 
 int main (){
 
-	std::cout << "Hello world!"<< std::endl;
+	int my_number;
+
+	
+	std::cout << "Please introduce a number:" << std::endl;
+
+	std::cin >> my_number;
+
+	std::cout << "The number you introduced is:" << std::endl;
+
+	std::cout << my_number << std::endl;
+
+	
 
 	return 0;
+
+
 }
