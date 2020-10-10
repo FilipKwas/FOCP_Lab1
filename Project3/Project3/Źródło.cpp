a04d488c0ce8d325939e3188a209bd501f7ed698
FilipKwas <filikwa678@student.polsl.pl>
2020-10-10 11:53:08 +0200

    Even/odd number printing

--- Project3/Project3/Źródło.cpp
+++ Project3/Project3/Źródło.cpp
@@ -4,18 +4,19 @@ int main (){
 
 	int my_number;
 
-	
 	std::cout << "Please introduce a number:" << std::endl;
 
 	std::cin >> my_number;
 
-	std::cout << "The number you introduced is:" << std::endl;
+	if (my_number % 2 == 0) {
+		std::cout << "This number is even" << std::endl;
+	} else {
+		std::cout << "This number is odd" << std::endl;
 
-	std::cout << my_number << std::endl;
-
+	}
 	
 
+	
 	return 0;
-
 
 }
